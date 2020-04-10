b8d115ec8457922b9ba21b9950f1b7819a322ee4
Miki <mikikuromoto0119@gmail.com>
2020-04-10 18:09:56 +0900

    initial commit

---
+++ Catering/Catering/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Catering
+//
+//  Created by Miki Kuromoto on 2020/03/04.
+//  Copyright © 2020 Sky-Angel. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Catering/Catering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Catering
+//
+//  Created by Miki Kuromoto on 2020/03/03.
+//  Copyright © 2020 Sky-Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
