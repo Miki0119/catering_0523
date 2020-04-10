dc9cff8c0bde562000f1b66bc3a081f2203afb3a
Miki <mikikuromoto0119@gmail.com>
2020-04-10 20:21:56 +0900

    collectionViewCellを作成追加

---
+++ Catering/Catering/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  Catering
+//
+//  Created by Miki Kuromoto on 2020/04/10.
+//  Copyright © 2020 Sky-Angel. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
